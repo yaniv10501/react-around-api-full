c3fdfd42a814241f0c27df153679ab537a2ca02c
Yaniv Schweitzer <yaniv10501@gmail.com>
2021-12-18 16:20:33 +0200

    .

--- backend/app.js
+++ backend/app.js
@@ -51,7 +51,7 @@ app.use(auth);
 app.use('/', usersRoutes);
 app.use('/', cardsRoutes);
 
-app.use(errors());
+app.use((req, res, next) => next(errors()));
 
 app.use((req, res, next) => new ResourceNotFound(req, res, next));
 
