d089b111cbf18a6eb1161e0e89841c6f02540649
Yaniv Schweitzer <yaniv10501@gmail.com>
2022-09-09 19:38:47 +0300

    signout

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -41,32 +41,31 @@ module.exports.getUserById = (req, res, next) => {
 };
 
 module.exports.createUser = (req, res, next) => {
-  const {
-    name,
-    about,
-    link,
-    email,
-    password,
-  } = req.body;
-  bcrypt.hash(password, 10)
-    .then((hash) => User.create({
-      name,
-      about,
-      link,
-      email,
-      password: hash,
-    })
-      .then((newUser) => res.status(201).json({
-        message: 'A new user has been created',
-        user: {
-          _id: newUser._id,
-          name: newUser.name,
-          about: newUser.about,
-          link: newUser.link,
-          email: newUser.email,
-        },
-      }))
-      .catch((error) => checkErrors(error, next)))
+  const { name, about, link, email, password } = req.body;
+  bcrypt
+    .hash(password, 10)
+    .then((hash) =>
+      User.create({
+        name,
+        about,
+        link,
+        email,
+        password: hash,
+      })
+        .then((newUser) =>
+          res.status(201).json({
+            message: 'A new user has been created',
+            user: {
+              _id: newUser._id,
+              name: newUser.name,
+              about: newUser.about,
+              link: newUser.link,
+              email: newUser.email,
+            },
+          })
+        )
+        .catch((error) => checkErrors(error, next))
+    )
     .catch((error) => checkErrors(error, next));
 };
 
@@ -79,15 +78,17 @@ module.exports.updateUser = (req, res, next) => {
     {
       new: true,
       runValidators: true,
-    },
+    }
   )
     .orFail(() => {
       throw new NotFoundError('User ID not found');
     })
-    .then((user) => res.json({
-      message: `User - ${userId} profile has been updated`,
-      user,
-    }))
+    .then((user) =>
+      res.json({
+        message: `User - ${userId} profile has been updated`,
+        user,
+      })
+    )
     .catch((error) => checkErrors(error, next));
 };
 
@@ -100,116 +101,141 @@ module.exports.updateAvater = (req, res, next) => {
     {
       new: true,
       runValidators: true,
-    },
+    }
   )
     .orFail(() => {
       throw new NotFoundError('User ID not found');
     })
-    .then((user) => res.json({
-      message: `User - ${userId} avatar has been updated`,
-      user,
-    }))
+    .then((user) =>
+      res.json({
+        message: `User - ${userId} avatar has been updated`,
+        user,
+      })
+    )
     .catch((error) => checkErrors(error, next));
 };
 
 module.exports.login = (req, res, next) => {
-  const {
-    JWT_SECRET = 'Secret-key',
-  } = process.env;
+  const { JWT_SECRET = 'Secret-key' } = process.env;
   const { email, password } = req.body;
   User.findOne({ email })
     .orFail(() => {
       throw new AuthorizationError('Incorrect email or password');
     })
     .select('+password')
-    .then((user) => bcrypt.compare(password, user.password)
-      .then(async (matched) => {
-        if (matched) {
-          const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '10s' });
-          const refreshToken = uuidv4();
-          const refreshJwt = jwt.sign({ _id: user._id, token: refreshToken }, JWT_SECRET, {
-            expiresIn: '7d',
-          });
-          await Tokens.create({
-            userId: user._id,
-            refreshTokens: [
-              {
-                token: refreshToken,
-                expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-              },
-            ],
-          })
-            .then(() => {
-              res.cookie('authorization', `Bearer ${token}`, {
-                maxAge: 1000 * 30,
-                httpOnly: true,
-                secure: true,
-                domain: 'around.yanivportfolio.com',
-              });
-              res.cookie('refreshToken', refreshJwt, {
-                maxAge: 1000 * 60 * 60 * 24 * 7,
-                httpOnly: true,
-                secure: true,
-                signed: true,
-                domain: 'around.yanivportfolio.com',
-              });
-              return res.json({
-                email: user.email,
-                name: user.name,
-              });
-            })
-            .catch((error) => {
-              if (
-                error.name !== 'MongoServerError' ||
-                !error.message.includes('userId_1 dup key')
-              ) {
-                checkErrors(error, next);
-                return;
-              }
-              Tokens.findOneAndUpdate(
-                { userId: user._id },
-                {
-                  $addToSet: {
-                    refreshTokens: {
-                      token: refreshToken,
-                      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-                    },
-                  },
-                }
-              )
-                .then(async (result) => {
-                  await result.refreshTokens.forEach(async (item) => {
-                    if (Date.now() >= item.expires) {
-                      await Tokens.findOneAndUpdate(
-                        { userId: user._id },
-                        {
-                          $pull: { refreshTokens: { token: item.token } },
-                        }
-                      );
-                    }
-                  });
-                  res.cookie('authorization', `Bearer ${token}`, {
-                    maxAge: 1000 * 30,
-                    httpOnly: true,
-                    secure: true,
-                    domain: 'around.yanivportfolio.com',
-                  });
-                  res.cookie('refreshToken', refreshJwt, {
-                    maxAge: 1000 * 60 * 60 * 24 * 7,
-                    httpOnly: true,
-                    secure: true,
-                    signed: true,
-                    domain: 'around.yanivportfolio.com',
-                  });
-                  return res.json({
-                    message: 'Successfully logged in',
-                  });
-                })
-                .catch((err) => checkErrors(err, next));
+    .then((user) =>
+      bcrypt
+        .compare(password, user.password)
+        .then(async (matched) => {
+          if (matched) {
+            const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '10s' });
+            const refreshToken = uuidv4();
+            const refreshJwt = jwt.sign({ _id: user._id, token: refreshToken }, JWT_SECRET, {
+              expiresIn: '7d',
             });
-        }
-        throw new AuthorizationError('Incorrect email or password');
-      })
-      .catch((error) => checkErrors(error, next)))
+            await Tokens.create({
+              userId: user._id,
+              refreshTokens: [
+                {
+                  token: refreshToken,
+                  expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+                },
+              ],
+            })
+              .then(() => {
+                res.cookie('authorization', `Bearer ${token}`, {
+                  maxAge: 1000 * 30,
+                  httpOnly: true,
+                  secure: true,
+                  domain: 'around.yanivportfolio.com',
+                });
+                res.cookie('refreshToken', refreshJwt, {
+                  maxAge: 1000 * 60 * 60 * 24 * 7,
+                  httpOnly: true,
+                  secure: true,
+                  signed: true,
+                  domain: 'around.yanivportfolio.com',
+                });
+                return res.json({
+                  email: user.email,
+                  name: user.name,
+                });
+              })
+              .catch((error) => {
+                if (
+                  error.name !== 'MongoServerError' ||
+                  !error.message.includes('userId_1 dup key')
+                ) {
+                  checkErrors(error, next);
+                  return;
+                }
+                Tokens.findOneAndUpdate(
+                  { userId: user._id },
+                  {
+                    $addToSet: {
+                      refreshTokens: {
+                        token: refreshToken,
+                        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+                      },
+                    },
+                  }
+                )
+                  .then(async (result) => {
+                    await result.refreshTokens.forEach(async (item) => {
+                      if (Date.now() >= item.expires) {
+                        await Tokens.findOneAndUpdate(
+                          { userId: user._id },
+                          {
+                            $pull: { refreshTokens: { token: item.token } },
+                          }
+                        );
+                      }
+                    });
+                    res.cookie('authorization', `Bearer ${token}`, {
+                      maxAge: 1000 * 30,
+                      httpOnly: true,
+                      secure: true,
+                      domain: 'around.yanivportfolio.com',
+                    });
+                    res.cookie('refreshToken', refreshJwt, {
+                      maxAge: 1000 * 60 * 60 * 24 * 7,
+                      httpOnly: true,
+                      secure: true,
+                      signed: true,
+                      domain: 'around.yanivportfolio.com',
+                    });
+                    return res.json({
+                      message: 'Successfully logged in',
+                    });
+                  })
+                  .catch((err) => checkErrors(err, next));
+              });
+          }
+          throw new AuthorizationError('Incorrect email or password');
+        })
+        .catch((error) => checkErrors(error, next))
+    )
+    .catch((error) => checkErrors(error, next));
+};
+
+module.exports.logout = (req, res, next) => {
+  const { _id } = req.user;
+  Tokens.deleteOne({ userId: _id })
+    .then(() => {
+      res.cookie('authorization', '', {
+        maxAge: 0,
+        httpOnly: true,
+        secure: true,
+        domain: 'around.yanivportfolio.com',
+      });
+      res.cookie('refreshToken', '', {
+        maxAge: 0,
+        httpOnly: true,
+        secure: true,
+        signed: true,
+        domain: 'around.yanivportfolio.com',
+      });
+      res.json({ message: 'Successfully logged out' });
+    })
     .catch((error) => checkErrors(error, next));
 };
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,23 +1,21 @@
 const router = require('express').Router();
 const { celebrateUpdateUser, celebrateUpdateAvatar } = require('../utils/celebrate');
 const {
-  getAllUsers, getUserById, updateUser, updateAvater,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  updateAvater,
+  logout,
 } = require('../controllers/users');
+
+router.post('/signout', logout);
 
 router.get('/users', getAllUsers);
 
 router.get('/users/me', getUserById);
 
-router.patch(
-  '/users/me',
-  celebrateUpdateUser,
-  updateUser,
-);
+router.patch('/users/me', celebrateUpdateUser, updateUser);
 
-router.patch(
-  '/users/me/avatar',
-  celebrateUpdateAvatar,
-  updateAvater,
-);
+router.patch('/users/me/avatar', celebrateUpdateAvatar, updateAvater);
 
 module.exports = router;
